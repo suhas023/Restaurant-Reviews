07d3d8a56c134f257b7ab5eaa18cade9b172a87a
suhas023 <notsuhas@gmail.com>
2018-10-14 13:33:11 +0530

    added activate event listener

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
-let cacheName = 'v1';
+let currentCacheName = 'vX';
+
 let resources = [
 	'/',
+	'/restaurant.html',
     '/js/dbhelper.js',
     '/js/main.js',
     '/js/restaurant_info.js',
+    '/data/restaurants.json',
     '/css/styles.css',
     '/img/1.jpg',
     '/img/2.jpg',
@@ -18,8 +21,46 @@ let resources = [
 ];
 
 addEventListener('install', event => {
+	skipWaiting();
 	event.waitUntil(
-		caches.open(cacheName)
+		caches.open(currentCacheName)
 		.then(cache => cache.addAll(resources))
 	);
+});
+
+addEventListener('fetch', event => {
+	//if restaurant page is requested
+	if(event.request.url.includes('restaurant.html?id=')) {
+  		event.respondWith(
+  			caches.match('/restaurant.html')
+  			.then(response => response || fetch(event.request))
+  		);
+  	}
+
+	else {
+		event.respondWith(
+			caches.match(event.request)
+		  	.then(response => {
+		    	return response || fetch(event.request).then(function(response) {
+        			return caches.open(currentCacheName).then(function(cache) {
+          				cache.put(event.request, response.clone());
+          				return response;
+          			});
+		  		});
+			})
+		);
+	}
+});
+
+addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys()
+        .then(cacheNames => {
+            Promise.all(
+            	cacheNames.filter(cacheName =>
+                	cacheName.startsWith('v') && cacheName != currentCacheName
+            	).map(cacheName => caches.delete(cacheName))
+            )
+        })
+    );
 });
