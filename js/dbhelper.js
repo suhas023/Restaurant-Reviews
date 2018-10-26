98d5ff851471da06279df0a04457359f8c298eaf
Suhas <notsuhas@gmail.com>
2018-10-26 15:12:39 +0530

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -150,7 +150,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`https://suhas023.github.io/Restaurant-Reviews/img/${restaurant.photograph}`);
   }
 
   /**
