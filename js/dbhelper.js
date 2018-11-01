e647fe86dfcde2e5ff12c884ead2ac960feb3f33
suhas023 <notsuhas@gmail.com>
2018-11-01 18:55:41 +0530

    changed data url

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    return `https://suhas023.github.io/Restaurant-Reviews/data/restaurants.json`;
+    return `http://localhost:${port}/data/restaurants.json`;
   }
 
   /**
@@ -150,7 +150,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`https://suhas023.github.io/Restaurant-Reviews/img/${restaurant.photograph}`);
+    return (`/img/${restaurant.photograph}`);
   }
 
   /**
